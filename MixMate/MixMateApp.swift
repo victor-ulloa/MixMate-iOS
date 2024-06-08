94a4d6a3665a347e22556730b3ce1d86e1d59325
Victor Ulloa <alejandroekas@gmail.com>
2024-06-08 00:29:23 -0400

    project created

---
+++ MixMate/MixMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MixMateApp.swift
+//  MixMate
+//
+//  Created by Victor Ulloa on 2024-06-08.
+//
+
+import SwiftUI
+
+@main
+struct MixMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
