f532b945e036f3d9f5d1547d95caab5b33531344
Victor Ulloa <alejandroekas@gmail.com>
2024-06-09 21:58:39 -0400

    created base tabs with homepage class

---
+++ MixMate/App/Homepage/Homepage.swift
@@ -0,0 +1,18 @@
+//
+//  Homepage.swift
+//  MixMate
+//
+//  Created by Victor Ulloa on 2024-06-09.
+//
+
+import SwiftUI
+
+struct Homepage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Homepage()
+}
