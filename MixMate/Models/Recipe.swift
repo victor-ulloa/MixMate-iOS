65d4834e274bdab8f89728eaff61d2e893dc6cba
Victor Ulloa <alejandroekas@gmail.com>
2024-06-10 18:59:45 -0400

    MIX-10 | created base recipe model

---
+++ MixMate/Models/Recipe.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe.swift
+//  MixMate
+//
+//  Created by Victor Ulloa on 2024-06-10.
+//
+
+import Foundation
+
+struct Recipe: Codable {
+    
+    let name: String
+    let shortDescription: String
+    let imageName: String
+    let imageURL: String
+    
+}
