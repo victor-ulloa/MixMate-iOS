1438e15933705eccb83401cd2879f3f0e0c835e5
Victor Ulloa <alejandroekas@gmail.com>
2024-11-08 23:00:41 -0500

    updating on main cocktail card

--- MixMate/App/Homepage/HomepageView.swift
+++ MixMate/App/Homepage/HomepageView.swift
@@ -17,15 +17,15 @@ struct HomepageView: View {
                 VStack(spacing: 25){
                     if let cocktail = viewModel.cocktails.first {
                         NavigationLink {
-                            RecipeDetailView(cocktail: cocktail)
+                            RecipeDetailView(cocktail: cocktail, onUpdate: onCocktailUpdated)
                         } label: {
                             ImageCard(cocktail: cocktail)
                                 .padding(.horizontal, 20)
                         }
                     }
                     RecipesCarousel(title: "Your Favourites!", cocktails: viewModel.favouritesCarousel, onUpdate: onCocktailUpdated)
-                    RecipesCarousel(title: "Summer time!", cocktails: viewModel.summerCarousel)
-                    RecipesCarousel(title: "But first coffee", cocktails: viewModel.coffeeCarousel)
+                    RecipesCarousel(title: "Summer time!", cocktails: viewModel.summerCarousel, onUpdate: onCocktailUpdated)
+                    RecipesCarousel(title: "But first coffee", cocktails: viewModel.coffeeCarousel, onUpdate: onCocktailUpdated)
                 }
                 .padding(.top)
             }
