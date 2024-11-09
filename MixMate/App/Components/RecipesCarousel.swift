ea0c6e0a1db0846a300de52c551f9644c30c0c8d
Victor Ulloa <alejandroekas@gmail.com>
2024-11-08 23:02:57 -0500

    ordering carousel alphabetically

--- MixMate/App/Components/RecipesCarousel.swift
+++ MixMate/App/Components/RecipesCarousel.swift
@@ -22,7 +22,7 @@ struct RecipesCarousel: View {
                 
                 ScrollView(.horizontal, showsIndicators: false) {
                     HStack(spacing: 0) {
-                        ForEach(cocktails, id: \.self) { cocktail in
+                        ForEach(cocktails.sorted(by: {($0.name ?? "") < ($1.name ?? "")}), id: \.self) { cocktail in
                             NavigationLink {
                                 RecipeDetailView(cocktail: cocktail, onUpdate: onUpdate)
                             } label: {
