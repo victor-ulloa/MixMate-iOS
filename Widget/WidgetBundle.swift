1b2eb51eb64213435c209cffef2e9c6ab1626c21
Victor Ulloa <alejandroekas@gmail.com>
2024-11-20 17:41:18 -0500

    created widget target

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Victor Ulloa on 2024-11-20.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Widget()
+        WidgetControl()
+    }
+}
---
+++ Widget/WidgetControl.swift
@@ -0,0 +1,54 @@
+//
+//  WidgetControl.swift
+//  Widget
+//
+//  Created by Victor Ulloa on 2024-11-20.
+//
+
+import AppIntents
+import SwiftUI
+import WidgetKit
+
+struct WidgetControl: ControlWidget {
+    var body: some ControlWidgetConfiguration {
+        StaticControlConfiguration(
+            kind: "com.mixmate.MixMateApp.Widget",
+            provider: Provider()
+        ) { value in
+            ControlWidgetToggle(
+                "Start Timer",
+                isOn: value,
+                action: StartTimerIntent()
+            ) { isRunning in
+                Label(isRunning ? "On" : "Off", systemImage: "timer")
+            }
+        }
+        .displayName("Timer")
+        .description("A an example control that runs a timer.")
+    }
+}
+
+extension WidgetControl {
+    struct Provider: ControlValueProvider {
+        var previewValue: Bool {
+            false
+        }
+
+        func currentValue() async throws -> Bool {
+            let isRunning = true // Check if the timer is running
+            return isRunning
+        }
+    }
+}
+
+struct StartTimerIntent: SetValueIntent {
+    static let title: LocalizedStringResource = "Start a timer"
+
+    @Parameter(title: "Timer is running")
+    var value: Bool
+
+    func perform() async throws -> some IntentResult {
+        // Start / stop the timer based on `value`.
+        return .result()
+    }
+}
